678ec74692c06b207cb21262ce7ec5094e80385a
FreddyCornil <fredc@mailo.com>
2024-11-15 10:59:53 +0100

    Update AddLocalUsers.ps1

--- AddLocalUsers.ps1
+++ AddLocalUsers.ps1
@@ -16,25 +16,42 @@
     # Ajoute la ligne de journal au fichier
     Add-Content -Path $FilePath -Value $logLine
 }
-Function Random-Password ($length = 10){    $punc = 46..46
+
+Function Random-Password ($length = 10)
+{
+    $punc = 46..46
     $digits = 48..57
-    $letters = 65..90 + 97..122
+    $letters = 65..90 + 97..122
+
     $password = get-random -count $length -input ($punc + $digits + $letters) |`
         ForEach -begin { $aa = $null } -process {$aa += [char]$_} -end {$aa}
     Return $password.ToString()
 }
 
-Function ManageAccentsAndCapitalLetters{    param ([String]$String)    
-    $StringWithoutAccent = $String -replace '[éèêë]', 'e' -replace '[àâä]', 'a' -replace '[îï]', 'i' -replace '[ôö]', 'o' -replace '[ùûü]', 'u'    $StringWithoutAccentAndCapitalLetters = $StringWithoutAccent.ToLower()    $StringWithoutAccentAndCapitalLetters
+Function ManageAccentsAndCapitalLetters
+{
+    param ([String]$String)
+    
+    $StringWithoutAccent = $String -replace '[éèêë]', 'e' -replace '[àâä]', 'a' -replace '[îï]', 'i' -replace '[ôö]', 'o' -replace '[ùûü]', 'u'
+    $StringWithoutAccentAndCapitalLetters = $StringWithoutAccent.ToLower()
+    $StringWithoutAccentAndCapitalLetters
 }
 
-$Path = "C:\Scripts"$CsvFile = "$Path\Users.csv"$LogFile = "$Path\Log.log"
-$Users = Import-Csv -Path $CsvFile -Delimiter ";" -Header "prenom","nom","fonction","description" -Encoding UTF8  | Select-Object -Skip 1
+$Path = "C:\Scripts"
+$CsvFile = "$Path\Users.csv"
+$LogFile = "$Path\Log.log"
+
+$Users = Import-Csv -Path $CsvFile -Delimiter ";" -Header "prenom","nom","fonction","description" -Encoding UTF8  | Select-Object -Skip 1
+
 foreach ($User in $Users)
 {
-    $Prenom = ManageAccentsAndCapitalLetters -String $User.prenom    $Nom = ManageAccentsAndCapitalLetters -String $User.Nom
+    $Prenom = ManageAccentsAndCapitalLetters -String $User.prenom
+    $Nom = ManageAccentsAndCapitalLetters -String $User.Nom
     $Name = "$Prenom.$Nom"
-    If (-not(Get-LocalUser -Name "$Prenom.$Nom" -ErrorAction SilentlyContinue))    {        $Pass = Random-Password        $Password = (ConvertTo-secureString $Pass -AsPlainText -Force)
+    If (-not(Get-LocalUser -Name "$Prenom.$Nom" -ErrorAction SilentlyContinue))
+    {
+        $Pass = Random-Password
+        $Password = (ConvertTo-secureString $Pass -AsPlainText -Force)
         $Description = "$($user.description) - $($User.fonction)"
         $UserInfo = @{
             Name                 = "$Prenom.$Nom"
